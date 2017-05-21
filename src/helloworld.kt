a5891f086fce1875393082f8b2304c56e1abb75c
Joel Handwell <joelhandwell@gmail.com>
2017-05-21 04:52:05 -0400

    Describe person

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -4,5 +4,9 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    print("World is composed of " + arrayOf("people", "organizations", "things", "events").joinToString())
+    print("World is composed of " + arrayOf("people", "organizations", "things", "events", "locations").joinToString())
+
+    println()
+
+    println("A person can be related to " + arrayOf("language", "academic discipline", "skill", "occupation").joinToString())
 }
